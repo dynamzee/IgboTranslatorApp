bdf0f70973fa63414292f1d3d47146a2c58065f1
dynamzee <dynamzee@gmail.com>
2026-02-19 11:55:45 +0100

    updated json file in mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -2,9 +2,9 @@ from engine.translator import IgboTranslator
 from engine.processor import get_greeting
 
 def run_app():
-    all_json_files = ["my_data/igbo_phrases_dict.json", "my_data/igbo_words_dict.json"]
+    all_json = ["my_data/igbo_phrases_dict.json", "my_data/igbo_words_dict.json"]
 
-    translator = IgboTranslator(all_json_files)
+    translator = IgboTranslator(all_json)
 
     print(get_greeting())
     print("<* WELCOME TO DYNA's IGBO TRANSLATOR APP *>")
