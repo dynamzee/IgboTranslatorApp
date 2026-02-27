9c8a79bb1c8ff55c54c5a3f9eff3abf0e932a8fb
dynamzee <dynamzee@gmail.com>
2026-02-27 20:11:43 +0100

    update all new changes

--- mainapp.py
+++ mainapp.py
@@ -6,13 +6,11 @@ import os
 app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_DEBUG'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_OWNER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('EMAIL_OWNER')
 
 mail = Mail(app)
 
