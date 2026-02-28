fb181177609d32b1397eab00af26aef7298f25db
dynamzee <dynamzee@gmail.com>
2026-02-28 23:20:52 +0100

    update all new changes

--- mainapp.py
+++ mainapp.py
@@ -1,20 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from engine.translator import IgboTranslator
-from flask_mail import Mail, Message
 import os
+import requests
 import threading
 
 app = Flask(__name__)
-
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_OWNER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-app.config['MAIL_TIMEOUT'] = 30
-
-mail = Mail(app)
 
 translator = IgboTranslator(paths={
     "all_json": "my_data/igbo_phrases_dict.json",
@@ -23,13 +13,21 @@ translator = IgboTranslator(paths={
     "reverse_simple_keys": "my_data/igbo_words_dict.json"
 })
 
-def send_async_email(app, msg):
-    with app.app_context():
-        try:
-            mail.send(msg)
-            print("GMAIL SUCCESS: Email sent!")
-        except Exception as e:
-            print(f"GMAIL ERROR: {e}")
+def send_to_slack(original, user_input):
+    webhook_url = os.environ.get('SLACK_WEBHOOK_URL')
+    if not webhook_url:
+        print("ERROR: SLACK_WEBHOOK_URL WASN'T FOUND IN RENDER!")
+        return
+
+    payload = {
+        "text": f"🚨 *FEEDBACK_CORRECTION_SUGGESTION!*\n*Original:* {original}\n*Correction:* {user_input}"
+    }
+
+    try:
+        response = requests.post(webhook_url, json=payload)
+        print(f"SLACK STATUS: {response.status_code}")
+    except Exception as e:
+        print(f"SLACK ERROR: {e}")
 
 @app.route('/')
 def index():
@@ -46,32 +44,18 @@ def feedback():
     data = request.json
     original = data.get('original')
     user_input = data.get('user_input')
-    admin_email = os.environ.get('EMAIL_OWNER')
 
     if not original or not user_input:
         return jsonify({"status": "error", "message": "Missing data"}), 400
 
-    msg = Message("New Igbo Translation Suggestion!",
-                  sender=admin_email,
-                  recipients=[admin_email])
-    msg.body = f"Original: {original}\nCorrection: {user_input}"
-
-    thread = threading.Thread(target=send_async_email, args=(app, msg))
+    thread = threading.Thread(target=send_to_slack, args=(original, user_input))
     thread.start()
 
-    return jsonify({"status": "success", "message": "Daalu! Suggestion received."})
+    return jsonify({"status": "success", "message": "Daalu!😊 Your suggestion has been sent!."})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
-
-
-
-
-
-
-
-
 
 
 
