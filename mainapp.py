4d7c38a3129d69ce78b9f284ff66dfb97bf20795
dynamzee <dynamzee@gmail.com>
2026-02-28 11:59:57 +0100

    update new changes

--- mainapp.py
+++ mainapp.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request, jsonify
 from engine.translator import IgboTranslator
+from flask_mail import Mail, Message
 import os
-import requests
 import threading
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_OWNER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_TIMEOUT'] = 30
+
+mail = Mail(app)
 
 translator = IgboTranslator(paths={
     "all_json": "my_data/igbo_phrases_dict.json",
@@ -13,20 +23,13 @@ translator = IgboTranslator(paths={
     "reverse_simple_keys": "my_data/igbo_words_dict.json"
 })
 
-def send_async_email(api_key, domain, admin_email, original, user_input):
-    try:
-        url = f"https://api.mailgun.net/v3/{domain}/messages"
-        auth = ("api", api_key)
-        data = {
-            "from": f"Translator App <mailgun@{domain}>",
-            "to": [admin_email],
-            "subject": "New Igbo Translation Suggestion!",
-            "text": f"Original: {original}\nCorrection: {user_input}"
-        }
-        response = requests.post(url, auth=auth, data=data)
-        print(f"MAILGUN STATUS: {response.status_code}")
-    except Exception as e:
-        print(f"MAILGUN ERROR: {e}")
+def send_async_email(app, msg):
+    with app.app_context():
+        try:
+            mail.send(msg)
+            print("GMAIL SUCCESS: Email sent!")
+        except Exception as e:
+            print(f"GMAIL ERROR: {e}")
 
 @app.route('/')
 def index():
@@ -43,18 +46,17 @@ def feedback():
     data = request.json
     original = data.get('original')
     user_input = data.get('user_input')
-
-    api_key = os.environ.get('MAILGUN_API_KEY')
-    domain = os.environ.get('MAILGUN_DOMAIN')
     admin_email = os.environ.get('EMAIL_OWNER')
 
     if not original or not user_input:
         return jsonify({"status": "error", "message": "Missing data"}), 400
 
-    thread = threading.Thread(
-        target=send_async_email,
-        args=(api_key, domain, admin_email, original, user_input)
-    )
+    msg = Message("New Igbo Translation Suggestion!",
+                  sender=admin_email,
+                  recipients=[admin_email])
+    msg.body = f"Original: {original}\nCorrection: {user_input}"
+
+    thread = threading.Thread(target=send_async_email, args=(app, msg))
     thread.start()
 
     return jsonify({"status": "success", "message": "Daalu! Suggestion received."})
@@ -62,6 +64,15 @@ def feedback():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
+
+
+
+
+
+
+
+
+
 
 
 
